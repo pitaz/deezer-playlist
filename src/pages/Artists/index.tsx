b50ac5bfb055dc16b1c62ae0122f728f0294807d
pitaz <peter.odekwo@gmail.com>
2023-09-29 01:01:11 +0100

    setup project

---
+++ src/pages/Artists/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Artist Page</h1>
+      <p>Welcome to the artist page!</p>
+    </div>
+  );
+};
+
+export default Home;
